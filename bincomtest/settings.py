0e8aecb42c0237ecf532b1bd7ed9cfa3ab9e98a7
wisdom <idriswisdom6@gmail.com>
2022-01-23 03:01:54 +0100

    css

--- bincomtest/settings.py
+++ bincomtest/settings.py
@@ -20,15 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-8^g4p0-c*c*(*&peb9vbpgi94d#-q2*s$_b&r5)a#xltb)8g=i'
+SECRET_KEY = 'django-insecure-8^g4p0-c*c*(*&peb9vbpgi94d#-q2*s$_b&r5)a#xltb)8g=i'
 
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','mybincomtest.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','mybincomtest.herokuapp.com','localhost']
 
 
 # Application definition
